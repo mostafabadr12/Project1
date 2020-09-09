f4c3c9ec8d3758fcda2c3894f0cef9de39df9e83
mostafa badr <mostafabadr@aucegypt.edu>
2020-09-09 17:41:48 +0200

    knabsack functions

---
+++ good_morning.cpp
@@ -0,0 +1,7 @@
+#include "hello_world.h"
+
+void hellow_world()
+
+{
+	cout << "good morning all!" << endl;
+}
---
+++ hello_world.cpp
@@ -0,0 +1,7 @@
+#include "hello_world.h"
+
+void hello_world()
+
+{
+	cout << "hello world!" << endl;
+}
